131af7a797c79a59ac60872aa4896e62712e5aed
Alexander <elegray888@hotmail.com>
2024-03-11 14:21:13 -0600

    added files

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -16,3 +16,33 @@ const styles = {
     }
 };
 
+const HomePage = () => {
+    return ( <div style={styles.container}>
+            <h2> Welcome to Our Project</h2>
+            <p>This project is designed to showcase our abilities to work with
+                React, including state management, routing, and interacting
+                with an API.</p>
+            <h3>Creators:</h3>
+            <table style={styles.table}>
+                <thread>
+                    <tr>
+                        <th style = {styles.th}>Name</th>
+                        <th style = {styles.th}>Role</th>
+                    </tr>
+                </thread>
+                <tbody>
+                    <tr>
+                        <td style={styles.td}>Alexander Lai</td>
+                        <td sytle={styles.td}>Developer</td>
+                    </tr>
+                    <tr>
+                        <td style={styles.td}>Odin Fox</td>
+                        <td sytle={styles.td}>Desinger & Lead Programmer</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            );
+    };
+
+export default HomePage;
---
+++ src/components/ProductList.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+import { fetchProducts } from "../services/apiService";
+import { useProductsContext } from "../context/ProductContext";
+
+const ProductList = () => {
+    const {products, setProducts} = useProductsContext()
+
+    useEffect(() => {
+    const getProducts = async () => {
+        const data = await fetchProducts();
+        setProducts(data.products);
+    }
+    getProducts();
+}, [setProducts]);
+
+return (
+    <div> 
+    {products.map((product) => (
+        <div key = {product.id}>{product.title}</div>
+    ))}
+    </div>
+);
+};
