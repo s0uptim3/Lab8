a5b61003dd8e5fc7c01b22d431044623270d1344
Alexander <elegray888@hotmail.com>
2024-03-11 14:02:25 -0600

    addded homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const styles = {
+    container: {
+        padding: '10px 1vw',
+    },
+    table: {
+        marginLeft: '2vw',
+        width: '100px',
+        borderCollapse: 'collapse'
+    },
+    th: {
+        vorderBottom: '1px solid #ddd',
+        padding: '8px',
+        textAlign: 'left',
+    }
+};
+
