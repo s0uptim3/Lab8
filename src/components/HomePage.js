c1d795cc1b9d59106693a1768f94f6b150669d97
Alexander <elegray888@hotmail.com>
2024-03-11 14:27:58 -0600

    applied VSC formatter

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,48 +1,50 @@
 import React from "react";
 
 const styles = {
-    container: {
-        padding: '10px 1vw',
-    },
-    table: {
-        marginLeft: '2vw',
-        width: '100px',
-        borderCollapse: 'collapse'
-    },
-    th: {
-        vorderBottom: '1px solid #ddd',
-        padding: '8px',
-        textAlign: 'left',
-    }
+	container: {
+		padding: "10px 1vw",
+	},
+	table: {
+		marginLeft: "2vw",
+		width: "100px",
+		borderCollapse: "collapse",
+	},
+	th: {
+		vorderBottom: "1px solid #ddd",
+		padding: "8px",
+		textAlign: "left",
+	},
 };
 
 const HomePage = () => {
-    return ( <div style={styles.container}>
-            <h2> Welcome to Our Project</h2>
-            <p>This project is designed to showcase our abilities to work with
-                React, including state management, routing, and interacting
-                with an API.</p>
-            <h3>Creators:</h3>
-            <table style={styles.table}>
-                <thread>
-                    <tr>
-                        <th style = {styles.th}>Name</th>
-                        <th style = {styles.th}>Role</th>
-                    </tr>
-                </thread>
-                <tbody>
-                    <tr>
-                        <td style={styles.td}>Alexander Lai</td>
-                        <td sytle={styles.td}>Developer</td>
-                    </tr>
-                    <tr>
-                        <td style={styles.td}>Odin Fox</td>
-                        <td sytle={styles.td}>Desinger & Lead Programmer</td>
-                    </tr>
-                </tbody>
-            </table>
-            </div>
-            );
-    };
+	return (
+		<div style={styles.container}>
+			<h2> Welcome to Our Project</h2>
+			<p>
+				This project is designed to showcase our abilities to work with React,
+				including state management, routing, and interacting with an API.
+			</p>
+			<h3>Creators:</h3>
+			<table style={styles.table}>
+				<thread>
+					<tr>
+						<th style={styles.th}>Name</th>
+						<th style={styles.th}>Role</th>
+					</tr>
+				</thread>
+				<tbody>
+					<tr>
+						<td style={styles.td}>Alexander Lai</td>
+						<td sytle={styles.td}>Developer</td>
+					</tr>
+					<tr>
+						<td style={styles.td}>Odin Fox</td>
+						<td sytle={styles.td}>Desinger & Lead Programmer</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
 
 export default HomePage;
--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const NotFoundPage = () => {
-    return(
-        <div>
-            <h2>404 not found</h2>
-            <p>The page you are looking for does not exist.</p>
-        </div>
-    );
+	return (
+		<div>
+			<h2>404 not found</h2>
+			<p>The page you are looking for does not exist.</p>
+		</div>
+	);
 };
 
 export default NotFoundPage;
