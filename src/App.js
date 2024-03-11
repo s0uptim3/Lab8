8e5e4b005649d42b53a6e820e8f5b4c33b07bd1f
s0uptim3 <odinboi345@gmail.com>
2024-03-11 14:32:32 -0600

    More Step C

--- src/App.js
+++ src/App.js
@@ -2,18 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ProductsProvider } from './context/ProductsContext'; // Adjust the import path as necessary
 import ProductList from './components/ProductList';
-<<<<<<< Updated upstream
 import NotFoundPage from './components/NotFoundPage';
 import HomePage from './components/HomePage';
-=======
-import ProductDetails from './components/ProductDetails';
-import ProductForm from './components/ProductForm';
-import NotFoundPage from './components/NotFoundPage';
-import HomePage from './components/HomePage';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faList, faPlus } from '@fortawesome/free-solid-svg-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> Stashed changes
 import './App.css';
 
 function App() {
@@ -23,7 +13,6 @@ function App() {
         <Router>
           <div>
             {/* Navigation Links */}
-<<<<<<< Updated upstream
             <nav>
               <ul>
                 <li>
@@ -44,44 +33,6 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="*" element={<NotFoundPage />} /> {/* Catch all other route */}
             </Routes>
-=======
-            <nav className="navbar navbar-expand navbar-light full-width">
-              <div className="container-fluid">
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav nav-full-width">
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home">
-                        <FontAwesomeIcon icon={faHome} />
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/products" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Product List">
-                        <FontAwesomeIcon icon={faList} />
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/product/add" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Add Product">
-                        <FontAwesomeIcon icon={faPlus} />
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
-
-
-            {/* Routes for different pages */}
-            <div className="container mt-3">
-              <Routes>
-                <Route path="/products" element={<ProductList />} />
-                <Route path="/product/:productId" element={<ProductDetails />} />
-                <Route path="/product/add" element={<ProductForm />} />
-                <Route path="/product/edit/:productId" element={<ProductForm />} />
-                <Route path="/" element={<HomePage />} />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </div>
->>>>>>> Stashed changes
           </div>
         </Router>
       </ProductsProvider>
