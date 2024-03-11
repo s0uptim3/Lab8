f9e2f0f4cb4a46c9dfb060a85300eacdd0175dd6
s0uptim3 <odinboi345@gmail.com>
2024-03-11 14:27:37 -0600

    Step C

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, {useEffect, useState} from "react";
 import { fetchProducts } from "../services/apiService";
 import { useProductsContext } from "../context/ProductContext";
@@ -23,3 +24,39 @@ return (
 };
 
 export default ProductList;
+=======
+// src/components/ProductList.js
+import React, { useEffect, useState } from 'react';
+import { fetchProducts } from '../services/apiService';
+import { useProductsContext } from '../context/ProductsContext';
+import { useNavigate } from 'react-router-dom';
+
+const ProductList = () => {
+  const { products, setProducts } = useProductsContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await fetchProducts();
+      setProducts(data.products);
+    };
+    getProducts();
+  }, [setProducts]);
+
+  return (
+    <div className="list-group">
+      {products.map((product, index) => (
+        <button
+          key={product.id}
+          className="list-group-item d-flex justify-content-between align-items-center hover-highlight"
+          onClick={() => navigate(`/product/${product.id}`)}
+        >
+          <span>{index + 1}. {product.title}</span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
+>>>>>>> Stashed changes
